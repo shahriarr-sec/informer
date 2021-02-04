4a9c564c4ca3e0053323b8ff9db9f87f53ac420d
shahriarr_sec <75485377+shahriarr-sec@users.noreply.github.com>
2021-02-04 13:25:52 +0600

    Update informer.sh
    Added meg in workflow.

--- informer.sh
+++ informer.sh
@@ -22,7 +22,7 @@ resolvers="/root/tools/informer/resolvers.txt"
 subdomain_enum(){
 for sub in $(cat $host);
 do
-mkdir -p /root/.gdrive/Recon-Data/$sub /root/.gdrive/Recon-Data/$sub/Subdomains /root/.gdrive/Recon-Data/$sub/ReconData /root/.gdrive/Recon-Data/$sub/ReconData/subjack /root/.gdrive/Recon-Data/$sub/ReconData/paramlist /root/.gdrive/Recon-Data/$sub/ReconData/nuclei /root/.gdrive/Recon-Data/$sub/ReconData/CustomWordlist /root/.gdrive/Recon-Data/$sub/Screenshots
+mkdir -p /root/.gdrive/Recon-Data/$sub /root/.gdrive/Recon-Data/$sub/Subdomains /root/.gdrive/Recon-Data/$sub/ReconData /root/.gdrive/Recon-Data/$sub/ReconData/subjack /root/.gdrive/Recon-Data/$sub/ReconData/paramlist /root/.gdrive/Recon-Data/$sub/ReconData/nuclei /root/.gdrive/Recon-Data/$sub/ReconData/CustomWordlist /root/.gdrive/Recon-Data/$sub/Screenshots /root/.gdrive/Recon-Data/$sub/Meg
 #dnscan
 echo "Listing Subdomains using dnscan..."
 python3 /root/tools/dnscan/dnscan.py -d $sub -t 100 -o /root/.gdrive/Recon-Data/$sub/Subdomains/dnscan_out.txt -w $wordlist_dns;
